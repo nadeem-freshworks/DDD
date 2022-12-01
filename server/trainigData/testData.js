4d9566bd8ea035d6608ce4d9c8a7d0433e2c05ab
Nagasuruthika <nagasuruthika.kannan@freshworks.com>
2022-12-01 20:12:03 +0530

    test data

--- server/trainigData/testData.js
+++ server/trainigData/testData.js
@@ -1,15 +1,20 @@
 var statusDocuments = [
-  `I love tacos.`,
-  `Dude, that burrito was epic!`,
-  `Holy cow, these nachos are so good and tasty.`,
-  `I am drooling over the awesome bean and cheese quesadillas.`
+  `app review process`, `approval`,`App Review Status Request`
+   `app review team`, `Code Review` , `Content review`, `QA review`,
+   `review process`, `Application blocked under review state`,
+    `still in the review process`,
+   `review the submission`, `under review`, `app will be reviewed`,`review comments`,
+   `reject the app in the code review`,`submit the app and get it reviewed`,
+   `app review exchanges`, `address the app review feedback`, `app resubmitted`,
+   `app has been approved`,`review is done`, `Wait for app review`,
+   `App Review process is stuck in testing`,`app is stuck in draft`,`reviewed and validated`,
+   `app state changed under review`, `review hasn’t cleared`, `code review changes`,
+   `resubmit app for review`,`published for review`,`in review`
 ]
  
 var regularDocuments = [
-  `Gross, worst taco ever.`,
-  `The buritos gave me horrible diarrhea.`,
-  `I'm going to puke if I eat another bad nacho.`,
-  `I'd rather die than eat those nasty enchiladas.`
+  `Any update on the same`,`Thanks in advance`,`regards`,`Thank you for reaching out`,
+  `Best regards`,`I hope this email finds you well`, `Thank you for your response`,
 ]
 
 exports  = {
