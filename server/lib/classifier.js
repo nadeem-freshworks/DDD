1c4c4d52687eb8459971b0f94ea4cad68b274bcc
Nagasuruthika <nagasuruthika.kannan@freshworks.com>
2022-12-02 08:46:41 +0530

    DDD

--- server/lib/classifier.js
+++ server/lib/classifier.js
@@ -2,23 +2,7 @@
 function getClassification(emailBody){
   const natural = require("natural");
   const classifier = new natural.BayesClassifier();
-  var statusDocuments = [ `app review process`, `approval`,`App Review Status Request`,
-      `app review team`, `Code Review` , `Content review`, `QA review`,
-      `review process`, `Application blocked under review state`,
-        `still in the review process`,
-      `review the submission`, `under review`, `app will be reviewed`,`review comments`,
-      `reject the app in the code review`,`submit the app and get it reviewed`,
-      `app review exchanges`, `address the app review feedback`, `app resubmitted`,
-      `app has been approved`,`review is done`, `Wait for app review`,
-      `App Review process is stuck in testing`,`app is stuck in draft`,`reviewed and validated`,
-      `app state changed under review`, `review hasnt cleared`, `code review changes`,
-      `resubmit app for review`,`published for review`,`in review`
-    ];
-    
-    var regularDocuments = [
-      `Any update on the same`,`Thanks in advance`,`regards`,`Thank you for reaching out`,
-      `Best regards`,`I hope this email finds you well`, `Thank you for your response`,
-    ];
+  const { regularDocuments, statusDocuments} = require('../trainingData/testData');
 
     for (let i = 0; i < statusDocuments.length; i++) {
       classifier.addDocument(tokenize(statusDocuments[i]), 'status');
